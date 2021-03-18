15e00d71012afecdf02259577ee15bdd69fe7c74
Zoe <891374900@qq.com>
2021-03-18 23:37:56 +0800

    update

---
+++ .temp/app-enhancers/global-components-3.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+Vue.component("Badge", () => import("/Users/zouyu/Desktop/zouyu/fish-vuePress/node_modules/@vuepress/theme-default/global-components/Badge"))
+Vue.component("CodeGroup", () => import("/Users/zouyu/Desktop/zouyu/fish-vuePress/node_modules/@vuepress/theme-default/global-components/CodeGroup"))
+Vue.component("CodeBlock", () => import("/Users/zouyu/Desktop/zouyu/fish-vuePress/node_modules/@vuepress/theme-default/global-components/CodeBlock"))
+
+
+export default {}
--- .temp/internal/app-enhancers.js
+++ .temp/internal/app-enhancers.js
@@ -1,6 +1,6 @@
 import m0 from "/Users/zouyu/Desktop/zouyu/fish-vuePress/.temp/app-enhancers/0.js"
 import m1 from "/Users/zouyu/Desktop/zouyu/fish-vuePress/.temp/app-enhancers/data-block.js"
-import m2 from "/Users/zouyu/Desktop/zouyu/fish-vuePress/.temp/app-enhancers/global-components-19.js"
+import m2 from "/Users/zouyu/Desktop/zouyu/fish-vuePress/.temp/app-enhancers/global-components-3.js"
 import m3 from "/Users/zouyu/Desktop/zouyu/fish-vuePress/.temp/app-enhancers/1.js"
 
 export default [
